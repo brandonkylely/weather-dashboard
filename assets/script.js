c38de6c1b08611b9c09c6561c7301704a565306c
Brandon <brandonkly@ucla.edu>
2022-11-05 18:49:09 -0700

    implemented functioning coordinate fetch

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,16 @@
-var apiKey = afc667415d83ab768de2ebb2e45133e9
-function getGeoLocation(lat, lon) {
-    return fetch(`api.openweathermap.org/data/2.5/forecast?lat={${lat}}&lon={${lon}}&appid={${apiKey}}`)
+var apiKey = "afc667415d83ab768de2ebb2e45133e9";
+function getWeather(lat, lon) {
+    return fetch(`api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`)
 }
-function getCoordinates(cityName, stateCode, countryCode) {
-    return fetch (`http://api.openweathermap.org/geo/1.0/direct?q={${cityName}},{${stateCode}},{${countryCode}}&limit={5}&appid={${apiKey}}`)
+function getCoordinates(cityName) {
+    return fetch (`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`)
 }
+
+getCoordinates('Los Angeles')
+.then(response => response.json())
+.then(data => {
+    document.body.textContent = JSON.stringify(data, null, 2)
+})
+.catch(error => {
+    document.body.textContent = error.message
+})
