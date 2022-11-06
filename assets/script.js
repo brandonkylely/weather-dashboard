d03b0811810cccba38f4aa69e4d25ffa6a4f206a
Brandon <brandonkly@ucla.edu>
2022-11-05 20:11:02 -0700

    added searchbar, and error message for locations not found

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,12 @@
 var apiKey = "afc667415d83ab768de2ebb2e45133e9";
+
+var input = document.querySelector('#cityInput')
+cityInput.addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        displayWeather(event.target.value)
+    } 
+})
+
 function getWeather({lat, lon}) {
     // var { lat, lon } = coordinates
     return fetch(`api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`)
@@ -13,28 +21,45 @@ function getCoordinates(cityName) {
 //     return response.json()
 // })
 
-getCoordinates('Los Angeles')
-.then(response => response.json())
-.then(data => {
-    // var lat = data[0].lat
-    // var lon = data[0].lon
-    // var {lat, lon} = data[0]
-    // console.log({lat, lon})
+function displayWeather(location) {
+    getCoordinates(location)
+    .then(response => response.json())
+    .then(data => {
 
-    getWeather({lat: data[0].lat, lon: data[0].lon})
-    .then(weatherResponse => weatherResponse.json())
 
-    .then(weatherData => {  
-        document.body.textContent = JSON.stringify(weatherData, null, 2)
+        // if (data.length === 0) {
+        //     var locationError = document.createElement('div')
+        //     locationError.textContent = `${location} not found.`
+        //     document.body.appendChild(locationError)
+        // } else {
+
+            // var lat = data[0].lat
+            // var lon = data[0].lon
+            // var {lat, lon} = data[0]
+            // console.log({lat, lon})
+            
+            getWeather({lat: data[0].lat, lon: data[0].lon})
+            .then(weatherResponse => weatherResponse.json())
+            
+            .then(weatherData => {  
+                // var weatherStatement = document.createElement('div')
+                // weatherStatement.textContent = `${weatherData.weather[0].main}: It is currently ${weatherData.weather[0].description}`
+                // document.body.appendChild(weatherStatement)
+                document.body.textContent = JSON.stringify(weatherData, null, 2)
+            })
+            .catch(error => {
+                document.body.textContent = error.message
+            })
+
+
+        // }
+
+
     })
     .catch(error => {
         document.body.textContent = error.message
     })
-})
-.catch(error => {
-    document.body.textContent = error.message
-})
-
+}
 
 // getCoordinates('Los Angeles')
 // .then(response => response.json())
