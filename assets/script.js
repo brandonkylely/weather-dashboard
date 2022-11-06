ca648069cdf27d2d5866b4b24b718e939f4b0e6c
Brandon <brandonkly@ucla.edu>
2022-11-05 19:38:01 -0700

    added comments for fetch calls

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,46 @@
 var apiKey = "afc667415d83ab768de2ebb2e45133e9";
-function getWeather(coordinates) {
-    var { lat, lon } = coordinates
-    return fetch(`api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+function getWeather({lat, lon}) {
+    // var { lat, lon } = coordinates
+    return fetch(`api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`)
 }
 function getCoordinates(cityName) {
-    return fetch (`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`)
+    return fetch (`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=3&appid=${apiKey}`)
 }
+
+// .then(response => response.json())
+// same result but above is preferred
+// .then(function(response) {
+//     return response.json()
+// })
 
 getCoordinates('Los Angeles')
 .then(response => response.json())
 .then(data => {
-    // var lat = data[0].lon
+    // var lat = data[0].lat
     // var lon = data[0].lon
-    var {lat, lon} = data[0]
-    getWeather({lat, lon})
+    // var {lat, lon} = data[0]
+    // console.log({lat, lon})
+
+    getWeather({lat: data[0].lat, lon: data[0].lon})
     .then(weatherResponse => weatherResponse.json())
+
     .then(weatherData => {  
         document.body.textContent = JSON.stringify(weatherData, null, 2)
-    })
     })
     .catch(error => {
         document.body.textContent = error.message
     })
+})
 .catch(error => {
     document.body.textContent = error.message
 })
+
+
+// getCoordinates('Los Angeles')
+// .then(response => response.json())
+// .then(data => {
+//     document.body.textContent = JSON.stringify(data, null, 2)
+// })
+// .catch(error => {
+//     document.body.textContent = error.message
+// })
