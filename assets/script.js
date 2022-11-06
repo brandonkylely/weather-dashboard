59736240231535af9e6cd46e8a3f3a7c2b6eb093
Brandon <brandonkly@ucla.edu>
2022-11-05 19:03:47 -0700

    added preliminary call for weather request with coordinates

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 var apiKey = "afc667415d83ab768de2ebb2e45133e9";
-function getWeather(lat, lon) {
+function getWeather(coordinates) {
+    var { lat, lon } = coordinates
     return fetch(`api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`)
 }
 function getCoordinates(cityName) {
@@ -9,8 +10,18 @@ function getCoordinates(cityName) {
 getCoordinates('Los Angeles')
 .then(response => response.json())
 .then(data => {
-    document.body.textContent = JSON.stringify(data, null, 2)
-})
+    // var lat = data[0].lon
+    // var lon = data[0].lon
+    var {lat, lon} = data[0]
+    getWeather({lat, lon})
+    .then(weatherResponse => weatherResponse.json())
+    .then(weatherData => {  
+        document.body.textContent = JSON.stringify(weatherData, null, 2)
+    })
+    })
+    .catch(error => {
+        document.body.textContent = error.message
+    })
 .catch(error => {
     document.body.textContent = error.message
 })
